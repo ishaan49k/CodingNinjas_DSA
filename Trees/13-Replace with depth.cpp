251a53b9d7ce55bd42c77b3636b86f2c718fff2c
Ishaan Kulkarni <74403067+ishaan49k@users.noreply.github.com>
2021-07-30 04:03:53 +0530

    Update 13-Replace with depth.cpp

--- Trees/13-Replace with depth.cpp
+++ Trees/13-Replace with depth.cpp
@@ -1,27 +1,5 @@
-/************************************************************
 
-    Following is the structure for the TreeNode class
-
-    template <typename T>
-    class TreeNode {
-     public:
-        T data;
-        vector<TreeNode<T>*> children;
-
-        TreeNode(T data) {
-            this->data = data;
-        }
-
-        ~TreeNode() {
-            for (int i = 0; i < children.size(); i++) {
-                delete children[i];
-            }
-        }
-    };
-
-************************************************************/
 void replaceWithDepthValueHelper(TreeNode<int>* root, int depth){
-    // handle root node:
     root->data = depth;
     
     // Call recursion of children of root node:
@@ -38,46 +16,5 @@ void replaceWithDepthValue(TreeNode<int>* root){
     
     replaceWithDepthValueHelper(root, 0);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
