770bc7f4152d1483d2dac76aba5e43b69638f1b9
Ishaan Kulkarni <74403067+ishaan49k@users.noreply.github.com>
2021-07-30 03:38:26 +0530

    Update Stack Using LL.cpp

--- Stacks & Queues/Stack Using LL.cpp
+++ Stacks & Queues/Stack Using LL.cpp
@@ -1,1 +1,45 @@
 
+
+class Stack {
+    Node *head;
+    int size;
+    public:
+        Stack() {
+            head = NULL;
+            size = 0;
+        }
+    int getSize() {
+    return size;
+    }
+    bool isEmpty() {
+    return size == 0;
+    }
+    void push(int element) {
+    Node *newnode = new Node(element);
+    if (head == NULL) {
+    head = newnode;
+    } else {
+    newnode->next = head;
+    head = newnode;
+    }
+    size++;
+    }
+    int pop() {
+    if (head == NULL) {
+    return -1;
+    }
+    int ans = head->data;
+    Node *temp = head;
+    head = head->next;
+    delete temp;
+    size--;
+    return ans;
+    }
+    int top() {
+    if (head == NULL) {
+    return -1;
+    }
+    return head->data;
+    }
+};
+
