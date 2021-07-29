e34aa5e9fd17071d50e649ddcd213c0c9caa2b96
Ishaan Kulkarni <74403067+ishaan49k@users.noreply.github.com>
2021-07-30 04:20:12 +0530

    Update 12-Level order traversal.cpp

--- Binary Trees/12-Level order traversal.cpp
+++ Binary Trees/12-Level order traversal.cpp
@@ -1,38 +1,18 @@
-/**********************************************************
-
-    Following is the Binary Tree Node class structure
-
-    template <typename T>
-    class BinaryTreeNode {
-        public :
-        T data;
-        BinaryTreeNode<T> *left;
-        BinaryTreeNode<T> *right;
-
-        BinaryTreeNode(T data) {
-            this -> data = data;
-            left = NULL;
-            right = NULL;
-        }
-    };
-
-***********************************************************/
-// Final solution
 #include<queue>
 
 void printLevelWise(BinaryTreeNode<int> *root){
     queue<BinaryTreeNode<int>*> pendingNodes;
-    pendingNodes.push(root); // after root, level gest over, so insert NULL in the queue
+    pendingNodes.push(root); 
     pendingNodes.push(NULL);
     
     while(pendingNodes.size() != 0){
         BinaryTreeNode<int>* front = pendingNodes.front();
         pendingNodes.pop();
         
-        if(front == NULL){ // if front==NULL , it mena swe have reached the ends of a level
+        if(front == NULL){ 
             cout << endl;
-            if(pendingNodes.size() != 0){ // when we encounter NULL, it means that all nodes of next level have
-                pendingNodes.push(NULL);   // been inserted in queue (check by dry running)
+            if(pendingNodes.size() != 0){
+                pendingNodes.push(NULL);
             }
         }else{
             cout << front->data << " ";
@@ -46,42 +26,3 @@ void printLevelWise(BinaryTreeNode<int> *root){
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
