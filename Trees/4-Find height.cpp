ee179a6ad966f0ddefc016097b7ef41b34e573db
Ishaan Kulkarni <74403067+ishaan49k@users.noreply.github.com>
2021-07-30 04:10:40 +0530

    Update 4-Find height.cpp

--- Trees/4-Find height.cpp
+++ Trees/4-Find height.cpp
@@ -1,25 +1,3 @@
-/************************************************************
- 
-    Following is the structure for the TreeNode class
-
-    template <typename T>
-    class TreeNode {
-     public:
-        T data;
-        vector<TreeNode<T>*> children;
-    
-        TreeNode(T data) {
-            this->data = data;
-        }
-    
-        ~TreeNode() {
-            for (int i = 0; i < children.size(); i++) {
-                delete children[i];
-            }
-        }
-    };
-
-************************************************************/
 
 int getHeight(TreeNode<int>* root){
 
@@ -33,29 +11,3 @@ int getHeight(TreeNode<int>* root){
     
     return ans + 1;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
