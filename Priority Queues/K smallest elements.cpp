6c2141efaace7c62616fd3f85b15cca844d291a0
Ishaan Kulkarni <74403067+ishaan49k@users.noreply.github.com>
2021-07-30 10:40:22 +0530

    Update and rename 4-K smallest elements.cpp to K smallest elements.cpp

--- Priority Queues/K smallest elements.cpp
+++ Priority Queues/K smallest elements.cpp
@@ -1,26 +1,15 @@
-// Final solution
-
 #include <queue>
 vector<int> kSmallest(int arr[], int n, int k){
-    // the pq max stores k smallest elements
     priority_queue<int> min;
-    
-    // insert first k elemensts into the pq
     for(int i=0; i<k; i++){
         min.push(arr[i]);
     }
-    
-    // loop runs on the remaining n-k elements of the array
     for(int i=k; i<n; i++){
         if(arr[i] < min.top()){
             min.pop();
             min.push(arr[i]);
         }
     }
-    
-    // now max stores the k smallest elements of the array
-    
-    // transfer all elements from the pq to vector
     vector<int> output;
     
     while(min.size() != 0){
