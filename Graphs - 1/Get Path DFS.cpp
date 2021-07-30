96f05f42337ca7b1b4ea488fc49b7f86c93be2e3
Ishaan Kulkarni <74403067+ishaan49k@users.noreply.github.com>
2021-07-30 11:44:28 +0530

    Update and rename 9-Get Path DFS.cpp to Get Path DFS.cpp

--- Graphs - 1/Get Path DFS.cpp
+++ Graphs - 1/Get Path DFS.cpp
@@ -9,24 +9,20 @@ vector<int>* getPathHelper(int** edges, int n, int sv, int ev, bool* visited){
         output->push_back(ev);
         return output;
     }
-    // if sv != ev, we first mark sv as visited
+
     visited[sv] = true;
-    
-    // check if adj vertices of sv have a path to ev
+
     for(int i=0; i<n; i++){
-        if(edges[sv][i] == 1 && visited[i] == false){ // call recursion on vertice which has edge with sv and is not visited
+        if(edges[sv][i] == 1 && visited[i] == false){
             vector<int>* smallPath = getPathHelper(edges, n, i, ev, visited);
-            
-            // in above recursion, if we get a path:
+
             if(smallPath != NULL){
                 smallPath->push_back(sv);
-                return smallPath;   // we now have 1 path from sv to ev. so we dont have to look for any other paths so just return this path
+                return smallPath;   
             }
         }
     }
     
-    // for loop over means sv!=ev and we dint get path from any of sv's adj vertices
-    // here we return NULL cuz it means there is no path from sv to ev
     return NULL;
     
 }
@@ -42,7 +38,6 @@ vector<int>* getPath(int** edges, int n, int sv, int ev){
     
     vector<int>* output = getPathHelper(edges, n, sv, ev, visited);
     
-    // dynammic arrays are deleted by the functions that make them
     delete [] visited;
     
     return output;
