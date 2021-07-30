e68316f9e62f9ade265308aa1f9e81c424ba6639
Ishaan Kulkarni <74403067+ishaan49k@users.noreply.github.com>
2021-07-30 11:40:03 +0530

    Update and rename 11-Is Connected.cpp to Is Connected.cpp

--- Graphs - 1/Is Connected.cpp
+++ Graphs - 1/Is Connected.cpp
@@ -3,10 +3,9 @@ using namespace std;
 
 
 void DFS(bool** graph, int v, bool* visited, int currentVertex){
-    // we have visited the currentVertex. so mark it as visited
+
     visited[currentVertex] = true;
-    
-    // call DFS on all the vertices adjacent to currentVertex
+
     for(int i=0; i<v; i++){
         if(i == currentVertex){
             continue;
@@ -24,10 +23,10 @@ bool isConnected(bool** graph, int v){
     // created a visited array
     bool* visited = new bool[v];
     
-    // perform DFS traversal on the graph starrting from the 0th vertex
+
     DFS(graph, v, visited, 0);
     
-    // now DFS is complete and the visisted array has been filled. check if there is even one false in it. if there is, graph is disconnected
+   
     for(int i=0; i<v; i++){
         if(visited[i] == false){
             return false;
