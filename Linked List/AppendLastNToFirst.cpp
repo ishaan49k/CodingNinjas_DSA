53891f8f79311a873b5b539e3326517e74c1168c
Ishaan Kulkarni <74403067+ishaan49k@users.noreply.github.com>
2021-07-30 18:38:47 +0530

    Update and rename 3-AppendLastNToFirst.cpp to AppendLastNToFirst.cpp

---
+++ Linked List/AppendLastNToFirst.cpp
@@ -0,0 +1,60 @@
+Node *appendLastNToFirst(Node *head, int n){
+    if(head==NULL || n==0){
+        return head;
+    }
+
+    int len=0;
+    Node*temp1 = head;
+    while(temp1!=NULL){
+        temp1=temp1->next;
+        len+=1;
+    }
+    
+    int curr=1;
+    Node* newTail=head;
+    while(curr<len-n){
+        newTail = newTail->next;
+        curr++;
+    }
+
+    Node* head2 = newTail->next;
+    newTail->next = NULL;
+    Node *linkingNode = head2;
+    while(linkingNode->next != NULL){
+        linkingNode = linkingNode->next;
+    }
+    
+    // Connect the 2 LL:
+    linkingNode->next = head;
+    
+    return head2;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
