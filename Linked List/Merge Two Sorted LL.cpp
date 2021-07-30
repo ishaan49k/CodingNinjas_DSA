a4725cda73394ff816f90854f9ce169d6d4c6c4d
Ishaan Kulkarni <74403067+ishaan49k@users.noreply.github.com>
2021-07-30 18:39:36 +0530

    Update and rename 5-Merge Two Sorted LL.cpp to Merge Two Sorted LL.cpp

--- Linked List/Merge Two Sorted LL.cpp
+++ Linked List/Merge Two Sorted LL.cpp
@@ -1,21 +1,3 @@
-/****************************************************************
- 
-    Following is the class structure of the Node class:
-
-        class Node
-        {
-        public:
-            int data;
-            Node *next;
-            Node(int data)
-            {
-                this->data = data;
-                this->next = NULL;
-            }
-        };
-
-*****************************************************************/
-
 Node *mergeTwoSortedLinkedLists(Node *head1, Node *head2){
     if(head1==NULL){
         return head2;
@@ -28,7 +10,6 @@ Node *mergeTwoSortedLinkedLists(Node *head1, Node *head2){
     Node* fh = NULL;
     Node* ft = NULL;
     
-    // First set the final head of merged LL:
     if(head1->data <= head2->data){
         ft = head1;
         fh = head1;
@@ -39,7 +20,6 @@ Node *mergeTwoSortedLinkedLists(Node *head1, Node *head2){
         head2 = head2->next;
     }
     
-    // Nowe we begin comparing:
     while(head1!=NULL && head2!=NULL){
         if(head1->data <= head2->data){
             ft->next = head1;
@@ -52,7 +32,6 @@ Node *mergeTwoSortedLinkedLists(Node *head1, Node *head2){
         }
     }
     
-    // Now if one of the LL has gotten exhasted:
     if(head1==NULL){
         ft->next = head2;
     }else{
