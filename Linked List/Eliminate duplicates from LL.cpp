c85722be3fccfbc360b4ae5a50765f1d328d2ec4
Ishaan Kulkarni <74403067+ishaan49k@users.noreply.github.com>
2021-07-30 18:39:10 +0530

    Update and rename 4-Eliminate duplicates from LL.cpp to Eliminate duplicates from LL.cpp

---
+++ Linked List/Eliminate duplicates from LL.cpp
@@ -0,0 +1,32 @@
+
+Node *removeDuplicates(Node *head){
+    if(head==NULL || head->next==NULL){
+        return head;
+    }
+
+    Node* t1 = head;
+    Node *t2 = head->next;
+
+    while(t2!=NULL){
+        if(t1->data != t2->data){
+            t1->next = t2;
+            t1 = t1->next;
+            t2 = t2->next;
+            continue;
+        }
+        if(t1->data == t2->data){
+            t2 = t2->next;
+            continue;
+        }
+    }
+    t1->next = t2;
+    return head;
+}
+
+
+
+
+
+
+
+
