8e26034c0f6cc3ba801c686997a60c95fffdbfdc
Ishaan Kulkarni <74403067+ishaan49k@users.noreply.github.com>
2021-07-30 11:25:36 +0530

    Update and rename 8-Pairs With Difference K.cpp to Pairs With Difference K.cpp

--- Hashmaps/Pairs With Difference K.cpp
+++ Hashmaps/Pairs With Difference K.cpp
@@ -1,22 +1,18 @@
 #include<unordered_maps>
 
 int getPairsWithDifferenceK(int* arr, int n, int k){
-    // create a map to stoore frequencies of all the elements in array
     unordered_maps<int, int> freq;
-    // key - element, value - its frequency
-    
-    int pairCount = 0; // final answer
-    
-    // iterate over every element in the array
+
+    int pairCount = 0;
+
     for(int i=0; i<n; i++){
         int a = arr[i] - k;
         int b = arr[i] + k;
         
-        if(freq.count(a) > 0){ // ie we have encountered a previously
+        if(freq.count(a) > 0){ 
             pairCount += freq[a];
         }
-        
-        // check if we have encountered b
+
         if(k != 0 && freq.count(b) > 0){
             pairCount += freq[b];
         }
@@ -28,68 +24,6 @@ int getPairsWithDifferenceK(int* arr, int n, int k){
     
     
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
