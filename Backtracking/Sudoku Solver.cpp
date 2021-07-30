4eaa22515903a9750cb67088a48c1b8282ada0f1
Ishaan Kulkarni <74403067+ishaan49k@users.noreply.github.com>
2021-07-30 11:34:25 +0530

    Update and rename 3-Sudoku Solver.cpp to Sudoku Solver.cpp

--- Backtracking/Sudoku Solver.cpp
+++ Backtracking/Sudoku Solver.cpp
@@ -2,12 +2,9 @@
 #include <cstring>
 using namespace std;
 
-#define N 9; // in code wherever N is there, it will get replaced with 9
+#define N 9;
 
-// takes in a grid and referance variables row and column
-// if we find empty location, return true, else false
 bool findEmptyLocation(int grid[N][N], int &row, int &col){
-    // iterate over the entire sudoku to find empty location
     for(int i=0; i<N; i++){
         for(int j=0; j<N; j++){
             if(grid[i][j] == 0){
@@ -24,9 +21,9 @@ bool findEmptyLocation(int grid[N][N], int &row, int &col){
 
 
 
-// checks if num is already present in row
+
 bool isSafeInRow(int grid[N][N], int row, int num){
-    for(int i=0; i<N; i++){ // for row, run a loop to check all cells in that row
+    for(int i=0; i<N; i++){ 
         if(grid[row][i] == num){
             return false;
         }
@@ -40,7 +37,7 @@ bool isSafeInRow(int grid[N][N], int row, int num){
 
 
 
-// checks if num is already present in col
+
 bool isSafeInCol(int grid[N][N], int col, int num){
     for(int i=0; i<N; i++){
         if(grid[i][col] == num){
@@ -53,9 +50,6 @@ bool isSafeInCol(int grid[N][N], int col, int num){
 
 
 
-
-
-// checks is num is already present in the 3*3 grid it is in
 bool isSafeInGrid(int grid[N][N], int row, int col, int num){
     int rowFactor = row - row%3;
     int colFactor = col - col%3;
@@ -75,11 +69,7 @@ bool isSafeInGrid(int grid[N][N], int row, int col, int num){
 
 
 
-
-
-// return true if it is allowed to place num at grid[row][col]
 bool isSafe(int grid[N][N], int row, int col, int num){
-// isSafe depends on 3 things - if num is unique in row, col and its 3*3 grid
     if(isSafeInRow(grid, row, num) && isSafeInCol(grid, col, num), isSafeInGrid(grid, row, col, num)){
         return true;
     }else{
@@ -91,32 +81,29 @@ bool isSafe(int grid[N][N], int row, int col, int num){
 
 
 
-
-// on solving the sudoku, return true
 bool solveSudoku(int grid[N][N]){
-    // find empty location
-    // initialize row & col. we pass them in findEmptyLocation fxn as referance variables and if we find an empty location, update the value of the referance variables which will reflect in this fxn also
+   
     int row;
     int col;
     if(findEmptyLocation(grid, row, col) == false){
         return true;
     }
     
-    // we found empty location. start filling it with suitable values
+    // found empty location. start filling it with suitable values
     for(int i=1; i<=N; i++){
-        // check if it is safe to fill this row,col with i
+
         if(isSafe(grid, row, col, i)){
             grid[row][col] = i;
-            if(solveSudoku(grid)){ // if by placing i at row,col, we are able to solve the remaing sudoku, return true cuz we have found the correct no. for row,col.
+            if(solveSudoku(grid)){ 
                 return true;
-            }else{   // with i at row,col if we were not able to solve the sudoku, then mark row,col as unresolved by marking it with 0. this is the backtracking step
+            }else{   
                 grid[row][col] = 0;
             }
         }
         
     }
     
-    return false;    // if we were not able to solve sudoku for any value from 1-9, return false
+    return false;   
     
     
 }
