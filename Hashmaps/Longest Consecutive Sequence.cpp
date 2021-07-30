3378935556fc4cf469199850c79b809be6498ed9
Ishaan Kulkarni <74403067+ishaan49k@users.noreply.github.com>
2021-07-30 11:24:05 +0530

    Update and rename 7-Longest Consecutive Sequence.cpp to Longest Consecutive Sequence.cpp

--- Hashmaps/Longest Consecutive Sequence.cpp
+++ Hashmaps/Longest Consecutive Sequence.cpp
@@ -1,24 +1,20 @@
-// Final code
-
 #include<vector>
 #include<unordered_map>
 
 vector<int> longestConsecutiveIncreasingSequence(int* arr, int n){
     
-    unordered_map<int, bool> visited; // stores all the elements we have encountered
-    // initialize the map
+    unordered_map<int, bool> visited; 
     unordered_map<int,int> index;
     for(int i=0; i<n; i++){
         visited[arr[i]] = true;
         index[arr[i]] = i;
     }
     
-    vector<int> output; // stores the final answer
+    vector<int> output;
     
     int maxLenght = 0;
     int startindex = -1;
-    
-    // iterate over every element in the array:
+
     for(int i=0; i<n; i++){
         int curr = arr[i];
         int currStartIndex = i;
@@ -31,15 +27,14 @@ vector<int> longestConsecutiveIncreasingSequence(int* arr, int n){
             visited[curr] = false;
             curr++;
         }
-        // check backward
+
         while(visited[tempcurr]==true){
             count++;
             visited[tempcurr] = false;
             currStartIndex = index[tempcurr];
             tempcurr--;
         }
-        
-        // count greater then length
+
         if(count>maxLenght){
             maxLenght = count;
             startindex = currStartIndex;
@@ -54,38 +49,6 @@ vector<int> longestConsecutiveIncreasingSequence(int* arr, int n){
     output.push_back(arr[startindex]+maxLenght-1);
     return output;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
