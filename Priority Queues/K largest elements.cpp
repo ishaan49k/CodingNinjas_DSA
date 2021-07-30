da614f622f584c19f64ea18d23a2462c59826af3
Ishaan Kulkarni <74403067+ishaan49k@users.noreply.github.com>
2021-07-30 10:41:37 +0530

    Update and rename 5-K largest elements.cpp to K largest elements.cpp

--- Priority Queues/K largest elements.cpp
+++ Priority Queues/K largest elements.cpp
@@ -1,27 +1,20 @@
-// Final solution
-
 #include <vector>
 #include<queue>
 
 vector<int> kLargest(int input[], int n, int k){
-    
-    // pq will store k largest elements
+
     priority_queue<int, vector<int>, greater<int>> pq;
-    
-    // put first k elemenst of array in pq
+
     for(int i=0; i<k; i++){
         pq.push(input[i]);
     }
-    
-    // iterate on remaining n-k elemets and compare each with smallest in pq
+
     for(int i=k; i<n; i++){
         if(input[i] > pq.top()){
             pq.pop();
             pq.push(input[i]);
         }
     }
-    
-    // now we have k largest elemensts stored in pq. copy them in a vector
     vector<int> output;
     
     while(pq.size() != 0){
