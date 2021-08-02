b64cfb5f57bd43fe4b2394b20a004cec0e5e2a0f
Ishaan Kulkarni <74403067+ishaan49k@users.noreply.github.com>
2021-08-02 23:03:02 +0530

    Update 15.Check AB.cpp

--- Recursion/15.Check AB.cpp
+++ Recursion/15.Check AB.cpp
@@ -1,1 +1,20 @@
+bool checkAB(char input[]) {
+	if(input[0]=='\0'){
+        return true;
+    }
+    if(input[0]!='a'){
+        return false;
+    }
+    if(input[1]!='\0' && input[2]!='\0'){
+        if(input[1]=='b' && input[2]=='b'){
+            return checkAB(input+3);
+        }
+    }
+    
+    return checkAB(input+1);  
+    
+}
+
+
+
 
