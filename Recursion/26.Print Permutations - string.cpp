a8d20d494e1e45827d8fb99387eb34981325fbe4
Ishaan Kulkarni <74403067+ishaan49k@users.noreply.github.com>
2021-08-03 17:50:07 +0530

    Update 26.Print Permutations - string.cpp

--- Recursion/26.Print Permutations - string.cpp
+++ Recursion/26.Print Permutations - string.cpp
@@ -1,20 +1,22 @@
-// do this
+// Final code
 
 #include <iostream>
 #include <string>
 using namespace std;
 
 
-// output stores the output tilll now
 void printPermutationsHelper(string input, string output){
     
     if(input.length() == 0){
         cout << output << endl;
     }
-    
-  
-    for(int i=0; i<=output.length(); i++){
-        printPermutationsHelper(input.substr(1), output.substr(0,i) + input[0] + output.substr(i));
+
+    for(int i=0; i<=input.length(); i++){
+        char firstChar = input[i]; 
+        string recInput = input.substr(0,i) + input.substr(i+1);
+        string recOutput = firstChar + output;
+        printPermutationsHelper(recInput, recOutput);
+        
     }
     
     
@@ -23,11 +25,26 @@ void printPermutationsHelper(string input, string output){
 
 
 
-
 void printPermutations(string input){
+    
    	string output = "";
     printPermutationsHelper(input, output);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
