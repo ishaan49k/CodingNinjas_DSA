71f24cdc77fad5c788d06cb7185f66471f479b93
Ishaan Kulkarni <74403067+ishaan49k@users.noreply.github.com>
2021-08-03 11:08:14 +0530

    Create 19.Print Subsets of Array.cpp

---
+++ Recursion/19.Print Subsets of Array.cpp
@@ -0,0 +1,25 @@
+void printSubsetsOfArrayHelper(int input[], int n, int output[], int m){
+
+    if(n==0){
+        for(int i=0; i<m; i+=1){
+            cout << output[i] << " ";
+        }
+        cout << endl;
+        return;
+    }
+
+    printSubsetsOfArrayHelper(input+1,n-1, output, m);
+    
+    output[m]=input[0]; 
+    printSubsetsOfArrayHelper(input+1,n-1,output,m+1);
+}
+
+
+void printSubsetsOfArray(int input[], int size){
+    int output[1000];
+    int m = 0;
+    printSubsetsOfArrayHelper(input, size, output, m);
+    return;
+}
+
+
