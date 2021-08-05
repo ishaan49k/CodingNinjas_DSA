0640f9d3d6521fa4a30056f8e7a59b89d8c4ca0b
Ishaan Kulkarni <74403067+ishaan49k@users.noreply.github.com>
2021-08-05 16:13:49 +0530

    Update Function to create LL.cpp

--- Linked List/Function to create LL.cpp
+++ Linked List/Function to create LL.cpp
@@ -1,14 +1,13 @@
-// call this fxn to create a LL. to stop making any more nodes, enter -1
 Node* takeInput(){
     int data;
     cin >> data;
     
-    Node* head = NULL;  // this is the head of the LL we need to return
-    Node* tail = NULL;  // this is the tail of LL we create. define this to make connecting new nodes at end cheaper
+    Node* head = NULL; 
+    Node* tail = NULL; 
     
     while(data != -1){
         Node* newNode = new Node(data);
-        if(head == NULL){   // if it is the first node, update the head. else connect newNode to end of LL
+        if(head == NULL){ 
             head = newNode;
             tail = newNode;
         }else{
