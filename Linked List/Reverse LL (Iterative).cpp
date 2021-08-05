dcaeae61a9f9ce8378ad1c249c36fec725010d3a
Ishaan Kulkarni <74403067+ishaan49k@users.noreply.github.com>
2021-08-05 23:52:05 +0530

    Update Reverse LL (Iterative).cpp

--- Linked List/Reverse LL (Iterative).cpp
+++ Linked List/Reverse LL (Iterative).cpp
@@ -5,6 +5,31 @@ Node *reverseLinkedList(Node *head){
     Node* c = head;
     Node* n = head->next;
     Node* p = NULL;
+	while(n != NULL){
+        c->next = p;
+        p = c;
+        c = n;
+        n = n->next;
+    }
+    
+    c->next = p;
+    return c;
+}
+
+
+
+OR
+
+
+
+
+Node *reverseLinkedList(Node *head){
+    if(head==NULL || head->next==NULL){
+        return head;
+    }
+    Node* c = head;
+    Node* n = head->next;
+    Node* p = NULL;
     while(c!=NULL){
         n =c->next;
         c->next = p; 
