2366cba2c96df4289e873cb3ffd75dca2352f345
Ishaan Kulkarni <74403067+ishaan49k@users.noreply.github.com>
2021-08-27 15:45:16 +0530

    Update Construct Tree from Postorder and Inorder.cpp

--- Binary Trees/Construct Tree from Postorder and Inorder.cpp
+++ Binary Trees/Construct Tree from Postorder and Inorder.cpp
@@ -1,4 +1,82 @@
+/***********************************************************
+	Following is the Binary Tree Node class structure
 
+	template <typename T>
+	class BinaryTreeNode {
+    	public : 
+    	T data;
+    	BinaryTreeNode<T> *left;
+    	BinaryTreeNode<T> *right;
+
+    	BinaryTreeNode(T data) {
+        	this -> data = data;
+        	left = NULL;
+        	right = NULL;
+    	}
+	};
+
+***********************************************************/
+// Final code
+
+BinaryTreeNode<int>* buildTreeHelper(int* postorder, int postStart, int postEnd, int* inorder, int inStart, int inEnd){
+	if(postStart > postEnd || inStart > inEnd){
+        return NULL;
+    }    
+    
+    int rootData = postorder[postEnd];
+    BinaryTreeNode<int>* root = new BinaryTreeNode<int>(rootData);
+    
+    // find the index of root in inorder
+    int k = 0;  // this stores the index of root in inorder
+    for(int i=inStart; i<=inEnd; i++){
+        if(inorder[i] == rootData){
+            k = i;
+            break;
+        }
+    }
+    
+    // call recursion to build the left and right subtrees and link it to the root
+    root->left = buildTreeHelper(postorder, postStart, postStart+k-inStart-1, inorder, inStart, k-1);
+    root->right = buildTreeHelper(postorder, postStart+k-inStart, postEnd-1, inorder, k+1, inEnd);
+    
+    return root;
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
+BinaryTreeNode<int>* buildTree(int *postorder, int postLength, int *inorder, int inLength){
+    int postStart = 0;
+    int postEnd = postLength - 1;
+    int inStart = 0;
+    int inEnd = inLength - 1;
+    
+    
+    // now we have the inorder, inStart, inEnd, postorder, postStart, postEnd of the tree we need to build
+    return buildTreeHelper(postorder, postStart, postEnd, inorder, inStart, inEnd);
+}
+
+
+
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+// another code that works:
 
 BinaryTreeNode<int>* buildTree(int *postorder, int postLength, int *inorder, int inLength) {
     
