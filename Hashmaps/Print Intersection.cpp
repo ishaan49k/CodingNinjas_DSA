515d9f1bb56328aae680de90a58d70b737fa2178
Ishaan Kulkarni <74403067+ishaan49k@users.noreply.github.com>
2021-09-07 08:51:29 +0530

    Update Print Intersection.cpp

--- Hashmaps/Print Intersection.cpp
+++ Hashmaps/Print Intersection.cpp
@@ -1,20 +1,22 @@
+// Final code
 
-#include<iostream>
-using namespace std;
-
-#include<unordered_map>
-
-void printIntersection(int*arr1, int size1, int *arr2, int size2){
-    unordered_map<int, int> freq;
-    for(int i=0; i<size1; i++){
-        freq[arr1[i]]++;
+#include <unordered_map>
+void intersection(int *arr1, int *arr2, int n, int m){
+    
+    // store frequency of all elements of arr1 in mp
+    unordered_map<int, int> mp;
+    for(int i=0; i<n; i++){
+        mp[arr1[i]]++;
     }
-    for(int i=0; i<size2; i++){
-        if(freq.count(arr2[i]) > 0){
-            cout << arr2[i];
-            freq[arr2[i]] --;
-            if(freq[arr2[i]] == 0){
-                freq.erase(arr2[i]);
+    
+    // check if element of arr2 exists in mp
+    sort(arr2, arr2 + m);  // did this step only so answer matches. without this is also fine
+    for(int i=0; i<m; i++){
+        if(mp.count(arr2[i])){
+            cout << arr2[i] << " ";
+            mp[arr2[i]]--;
+            if(mp[arr2[i]] == 0){
+                mp.erase(arr2[i]);
             }
         }
     }
